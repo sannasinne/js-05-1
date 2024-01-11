7974655bb97a2217700959aee623b93ad8827b89
sannasinne <sanna@keistinen.fi>
2024-01-11 15:03:29 +0200

    If else statement added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
 fans.addEventListener("mouseover", function () {
   fans.innerText = "123K";
@@ -12,4 +16,16 @@ fish.addEventListener("mouseover", function () {
 
 pets.addEventListener("mouseover", function () {
   pets.innerText = "20K";
+});
+
+toggle.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    h1.innerText = "Stealth Quincy";
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    h1.innerText = "Party Quincy";
+  }
 });
